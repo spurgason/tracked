b16b99677cfb44ba3fbd67c7fe72da4bcf7e150d
spurgason <shanepurgason.98@gmail.com>
2022-07-01 00:17:38 -0400

    added basic routes for users

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const characterRoutes = require("./characterRoutes");
+const userRoutes = require("./userRoutes");
 
 router.use("/character", characterRoutes);
+router.use("/users", userRoutes);
 
 module.exports = router;
