9638d2a7c7d93f0306bf31e79248ea21c00275ef
Shane <shanepurgason.98@gmail.com>
2022-07-24 23:51:31 -0400

    added basic user login, no auth

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -44,7 +44,17 @@ const registerUser = asyncHandler(async (req, res) => {
 });
 
 const loginUser = asyncHandler(async (req, res) => {
-  res.json({ message: "User login" });
+  const { email, password } = req.body;
+
+  // Check for user email
+  const user = await User.findOne({ email });
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.json({ _id: user.id, name: user.name, email: user.email });
+  } else {
+    res.status(400);
+    throw new Error("Invalid credentials");
+  }
 });
 
 const userData = asyncHandler(async (req, res) => {
